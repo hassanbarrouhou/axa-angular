e3d7e15f66212be22a39853bdfba09ceaf62f467
hassan <my.technologies.lab@gmail.com>
2020-07-16 08:34:19 +0200

    test
    test

--- Axa-Assesment/src/app/app-routing.module.ts
+++ Axa-Assesment/src/app/app-routing.module.ts
@@ -1,11 +1,23 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { GnomTownComponent } from './brastlewark/gnomTown/gnom-town.component';
+import { BrastlewarkComponent } from './brastlewark/component/brastlewark.component';
+import { UserProfileComponent } from './brastlewark/component/user-profile/user-profile.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+ // { path: 'brastlewark', loadChildren: './brastlewark/shared/brastlewark.module#BrastlewarkRouting' }
+//{ path: 'brastlewark', component: GnomTownComponent, pathMatch: 'full' }
 
-@NgModule({
-  imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
-})
-export class AppRoutingModule { }
+{
+  path: '', component: GnomTownComponent, children: [
+    { path: '', redirectTo: BrastlewarkComponent.Route, pathMatch: 'full'},
+    { path: BrastlewarkComponent.Route,  component: BrastlewarkComponent, pathMatch: 'full' },
+    { path: 'user/:id',  component: UserProfileComponent, pathMatch: 'full' },
+    ]
+}
+
+
+];
+
+export const AppRoutingModule = RouterModule.forRoot(routes); { }
--- Axa-Assesment/src/app/app.module.ts
+++ Axa-Assesment/src/app/app.module.ts
@@ -1,17 +1,35 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { GnomTownComponent } from './brastlewark/gnomTown/gnom-town.component';
+import { CommonModule } from '@angular/common';
+import { HttpClientModule } from '@angular/common/http';
+//import { FlexLayoutModule } from '@angular/flex-layout';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { BrastlewarkComponent } from './brastlewark/component/brastlewark.component';
+import { UserProfileComponent } from './brastlewark/component/user-profile/user-profile.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    BrastlewarkComponent,
+    GnomTownComponent,
+    UserProfileComponent
   ],
   imports: [
+    CommonModule,
     BrowserModule,
-    AppRoutingModule
+    HttpClientModule,
+    AppRoutingModule,
+    ReactiveFormsModule,
+    FormsModule,
+ //   FlexLayoutModule
   ],
+  exports: [
+    HttpClientModule,
+  ],
+
   providers: [],
   bootstrap: [AppComponent]
 })
